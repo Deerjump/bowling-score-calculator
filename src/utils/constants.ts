0a34d53b92dd62d83ec74354a9ff856d82b07a61
jbillman <billman377@gmail.com>
2023-02-27 13:28:54 -0700

    Extract constants to common file

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const STRIKE = 'X';
+export const SPARE = '/';
+export const FAULT = 'F';
+export const VALID_INPUT = [
+  STRIKE,
+  STRIKE.toLowerCase(),
+  SPARE,
+  FAULT,
+  FAULT.toLowerCase(),
+];
