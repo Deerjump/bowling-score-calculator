98e075423f723dd617585e00cde6045d68a1fa14
jbillman <billman377@gmail.com>
2023-02-26 22:30:33 -0700

    Added inputs on scoreboard

--- src/Frame.tsx
+++ src/Frame.tsx
@@ -1,19 +1,25 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './Scoreboard.module.css';
 
 type FrameProps = {
   roundNumber: number;
   firstScore?: number | string;
   secondScore?: number | string;
-  total?: number
+  total?: number;
 };
 
 function Frame({ roundNumber, firstScore, secondScore, total }: FrameProps) {
+ 
   return (
-    <div className={styles.frame} >
-      <div className={`${styles.frameHeader} ${styles.centeredText}`}>{roundNumber}</div>
+    <div className={styles.frame}>
+      <div className={`${styles.frameHeader} ${styles.centeredText}`}>
+        {roundNumber}
+      </div>
       <div className={styles.score}>
-        <div className={styles.centeredText}>{firstScore}</div>
-        <div className={`${styles.innerBox} ${styles.centeredText}`}>{secondScore}</div>
+        <input className={styles.centeredText} maxLength={1} defaultValue={firstScore}/>
+        <div className={styles.innerBox}>
+          <input className={styles.centeredText} defaultValue={secondScore}/>
+        </div>
         <div className={`${styles.total} ${styles.centeredText}`}>{total}</div>
       </div>
     </div>
