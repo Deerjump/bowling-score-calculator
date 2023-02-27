8548dad78f7a5bf403d21da6949c9fd84f071759
jbillman <billman377@gmail.com>
2023-02-27 13:29:46 -0700

    Added updateScore method to pass to children

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 import ResetButton from './ResetButton';
 import Scoreboard from './Scoreboard';
-
-const STRIKE = 'X';
-const SPARE = '/';
+import { FAULT, SPARE, STRIKE } from './utils/constants';
 
 const data: RoundScore[] = [
   { first: 10, second: 0 },
@@ -20,39 +18,57 @@ const data: RoundScore[] = [
 
 function App() {
   const [scores, setScores] = useState<RoundScore[]>(data);
-  
+
   const resetScores = () => {
     setScores([]);
-  }
+  };
 
-  function changeScore(round: number, score: RoundScore) {
-    const scoresCopy = [...scores];
+  function updateScore(round: number, newScore: RoundScore) {
+    console.log('Updating score', round, newScore);
+    // TODO: Check for fully empty scores
+    const index = round - 1;
+    const newScores = [...scores];
+    const oldScore = newScores[index];
 
-    scoresCopy[round] = score;
+    newScores[index] = { ...oldScore, ...newScore };
+    newScores[index] =
+      newScore.first === undefined && newScore.second === undefined
+        ? {}
+        : { ...oldScore, ...newScore };
+
+    setScores(newScores);
   }
 
   function calculateScore(scores: RoundScore[]): RoundScore[] {
     const totaledScores = [...scores];
-    let total = 0; 
+    let total = 0;
     for (const score of totaledScores) {
-      total += (score.first ?? 0 )+ (score?.second ?? 0);
+      if (score.first === undefined && score.second === undefined)
+        return totaledScores;
+      total +=
+        score?.first === 10 ? 10 : (score?.first ?? 0) + (score?.second ?? 0);
       score.total = total;
     }
-    
+
     return totaledScores;
   }
 
   function convertScoreToSymbol(score: RoundScore): DisplayScore {
-    if (score.first === 10) return { ...score, first: undefined, second: STRIKE };
+    if (score.first === 10)
+      return { ...score, first: undefined, second: STRIKE };
     const total = (score?.first ?? 0) + (score?.second ?? 0);
-    if (total === 10 ) return { ...score, second: SPARE };
-    return score; 
+    if (total === 10) return { ...score, second: SPARE };
+    return score;
   }
 
   return (
     <>
-      <Scoreboard scores={calculateScore(scores).map(convertScoreToSymbol)} />
-      <ResetButton resetScores={resetScores}/>
+      <Scoreboard
+        updateScore={updateScore}
+        scores={calculateScore(scores).map(convertScoreToSymbol)}
+      />
+      <ResetButton resetScores={resetScores} />
+      <button onClick={() => console.log(scores)}> Log Scores </button>
     </>
   );
 }
